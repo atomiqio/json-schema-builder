2cb061b9f7fab545892f3c43db2ce0ba5e43e347
Peter Svetlichny <petersvetlichny@gmail.com>
2015-07-30 15:16:23 -0700

    added not

---
+++ src/lib/Not.js
@@ -0,0 +1,32 @@
+import Keyword from './Keyword';
+import Schema from './Schema';
+
+export default class Not extends Keyword {
+	constructor(value) {
+		super();
+		this.value = value;
+	}
+
+	get value() {
+		return this._value;
+	}
+
+	set value(value) {
+		if (typeof value != 'object' || !(value instanceof Schema)) {
+			throw new Error('value must be an object and must be a valid JSON schema');
+		}
+
+		this._value = value;
+	}
+
+	build(context) {
+		context = context || {};
+
+		const value = (this.value instanceof Schema)
+			? this.value.build({})
+			: this.value;
+
+		context['not'] = value;
+		return context;
+	}
+}
