61beb04d9e08d551754b550c01c188a243e6e51a
Peter Svetlichny <petersvetlichny@gmail.com>
2015-07-30 17:55:26 -0700

    added minLength and pattern

---
+++ src/lib/MinLength.js
@@ -0,0 +1,27 @@
+import StringKeyword from './StringKeyword';
+
+export default class MinLength extends StringKeyword {
+	constructor(value) {
+		super();
+		this.value = value;
+	}
+
+	get value() {
+		return this._value;
+	}
+
+	set value(value) {
+		if (value >= 0 && Number.isInteger(value)) {
+			this._value = value;
+		} else {
+			throw new Error('value must be an integer and greater than or equal to zero');
+		}
+	}
+
+	build(context) {
+		context = context || {};
+
+		context.minLength = this.value;
+		return context;
+	}
+}
---
+++ src/lib/Pattern.js
@@ -0,0 +1,27 @@
+import StringKeyword from './StringKeyword';
+
+export default class Pattern extends StringKeyword {
+	constructor(value) {
+		super();
+		this.value = value;
+	}
+
+	get value() {
+		return this._value;
+	}
+
+	set value(value) {
+		if (typeof value === 'string') {
+			this._value = value;
+		} else {
+			throw new Error('value must be a string and should be a valid regular expression');
+		}
+	}
+
+	build(context) {
+		context = context || {};
+
+		context.pattern = this.value;
+		return context;
+	}
+}
