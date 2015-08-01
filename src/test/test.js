f7d188864394df7d01c0bcab5dadfd7f110f262d
Peter Svetlichny <petersvetlichny@gmail.com>
2015-07-31 18:49:25 -0700

    removed duplicate patternProperties test

--- src/test/test.js
+++ src/test/test.js
@@ -358,12 +358,6 @@ describe('object keywords', () => {
       return schema;
     });
 
-    // equivalent
-    test('patternProperties', 'patternProperties validates properties matching a regex', () => {
-      const schema = json.schema().patternProperty({ 'f.*o': json.integer() });
-      return schema;
-    });
-
     test('patternProperties', 'multiple simultaneous patternProperties are validated', () => {
       const schema = json.schema()
           .patternProperty('a*', json.integer())
